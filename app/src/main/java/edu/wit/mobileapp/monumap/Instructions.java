3e19465a3443fb4a6af04e705c0d2ca51599fc7f
arce-cs <arcec-cs@gmail.com>
2020-04-20 17:23:38 -0400

    patching runtime error: java.lang.NoClassDefFoundError: Failed resolution of: Landroidx/localbroadcastmanager/content/LocalBroadcastManager; used jetifer to convert from androidv7/androdroidv4 to androidx so broadcast manager would work. related to bt services
    https://stackoverflow.com/questions/52033810/can-i-use-library-that-used-android-support-with-androidx-projects

--- app/src/main/java/edu/wit/mobileapp/monumap/Instructions.java
+++ app/src/main/java/edu/wit/mobileapp/monumap/Instructions.java
@@ -1,11 +1,10 @@
 package edu.wit.mobileapp.monumap;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.speech.tts.TextToSpeech;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
